b5c9ce453b1050e050b475466a782ff3d15feeb9
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-04-29 16:44:10 +0200

    data

--- exercise05/main.py
+++ exercise05/main.py
@@ -19,7 +19,10 @@ if runtype == 1: # Parabolic
 
     name = "parabolic"
     parabolic = Parabolic()
+    tmax = 0.5
 
+    # Test of parameters
+    
     hn = [20, 40, 80]
 
     for i, h in enumerate(hn):
@@ -33,7 +36,7 @@ if runtype == 1: # Parabolic
                             activation_fn = nn.Tanh(),
                             N = N,
                             M = M,
-                            tmax = 0.5,
+                            tmax = tmax,
                             verbose = False,
                             pde = parabolic)
 
@@ -48,6 +51,67 @@ if runtype == 1: # Parabolic
             mdic = {"U": U, "T": T, "X": X, "loss": loss, "x": x, "t": t}
 
             savemat(f"results/{name}_{i}_{j}.mat", mdic)
+    
+    # Convergence test
+
+    M = 10000
+
+    H = [2,40,40,40,40,40,1];
+
+    for N in np.round(np.linspace(10,200,10)):
+        
+        N = int(N)
+
+        torch.manual_seed(1)
+
+        model = ODESolver(hidden_dimensions = H,
+                        activation_fn = nn.Tanh(),
+                        N = N,
+                        M = M,
+                        tmax = tmax,
+                        verbose = False,
+                        pde = parabolic)
+
+        N_EPOCHS = 3000
+        loss = model.train(N_EPOCHS)
+
+        U = model.predict().cpu().detach().numpy().reshape([N,M]).T
+        X = model.X.detach().cpu().numpy().reshape([N,M]).T
+        T = model.T.detach().cpu().numpy().reshape([N,M]).T
+        x = model.x.detach().cpu().numpy()
+        t = model.t.detach().cpu().numpy()
+        mdic = {"U": U, "T": T, "X": X, "loss": loss, "x": x, "t": t}
+
+        savemat(f"results/{name}_convergence_spatial_N_{N}.mat", mdic)
+
+    for N in np.round(np.linspace(10,200,10)):
+        N = int(N)
+        h = 2/N
+        k = h**2
+        M = int(np.ceil(tmax/k))
+
+        torch.manual_seed(1)
+
+        model = ODESolver(hidden_dimensions = H,
+                        activation_fn = nn.Tanh(),
+                        N = N,
+                        M = M,
+                        tmax = tmax,
+                        verbose = False,
+                        pde = parabolic)
+
+        N_EPOCHS = 3000
+        loss = model.train(N_EPOCHS)
+
+        U = model.predict().cpu().detach().numpy().reshape([N,M]).T
+        X = model.X.detach().cpu().numpy().reshape([N,M]).T
+        T = model.T.detach().cpu().numpy().reshape([N,M]).T
+        x = model.x.detach().cpu().numpy()
+        t = model.t.detach().cpu().numpy()
+        mdic = {"U": U, "T": T, "X": X, "loss": loss, "x": x, "t": t}
+
+        savemat(f"results/{name}_convergence_combined_N_{N}.mat", mdic)
+
 
 elif runtype == 2: # Hyperbolic
 
