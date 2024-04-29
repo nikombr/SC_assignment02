6a88dc30c22c91716d4647488ab3e05c477d812c
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-04-29 20:26:11 +0200

    hej

---
+++ exercise05/submit.sh
@@ -0,0 +1,18 @@
+#BSUB -J pythonSC # name
+#BSUB -o pythonSC_%J.out # output file
+#BSUB -q gpuh100
+#BSUB -n 32 ## cores
+#BSUB -R "rusage[mem=1GB]" 
+#BSUB -W 500 # useable time in minutes
+##BSUB -N # send mail when done
+#BSUB -R "span[hosts=1]"
+
+
+module load python3/3.10.13
+pip3 install torch
+pip3 install tqdm
+pip3 install scipy
+
+python3 main.py 1
+python3 main.py 2
+python3 main.py 3
